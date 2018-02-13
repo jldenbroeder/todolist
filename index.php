bceddd185f4b38b0bd2f7cca90064f46ce97a8c3
jldenbroeder <jldenbroeder@gmail.com>
2018-02-13 09:51:21 +0100

    ajout de index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <style>
+  p {
+    color: rgb(67, 69, 101);
+  }
+
+  h4 {
+    color: rgb(171, 92, 0);
+  }
+  </style>
+  <title>PHP / To-do list</title>
+</head>
+
+<body>
+  <p><a href="https://github.com/becodeorg/Swartz-promo-3/blob/master/Parcours/06-PHP/php-formulaires.md" target="_blank">Sanitisation et Validation</a> </p>
+  <h4>To do list</h4>
+  <p><a href="formulaire.php">Formulaire.php</a></p>
+  
+</body>
+</html>
